bff07cd10bfa99ab218481fa53bc9da2004a5f48
Seo BoHyeong <dontsay_no@nate.com>
2018-03-28 13:42:06 +0900

    - Change SDK to Windows Kinect v2 SDK - build Depth Camera for Coordinator 1 and Coordinator 2

--- Coordinator/MainWindow.xaml.cs
+++ Coordinator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
 
 namespace Coordinator
 {
@@ -19,10 +20,63 @@ namespace Coordinator
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
+
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += OnLoaded;
         }
+
+        private ImageSource ProcessFrame(DepthFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+            ushort[] pixelData = new ushort[width * height];
+            byte[] pixels = new byte[width * height * (format.BitsPerPixel + 7) / 8];
+            frame.CopyFrameDataToArray(pixelData);
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < pixelData.Length; ++depthIndex)
+            {
+                ushort depth = pixelData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+                pixels[colorIndex++] = intensity; // Blue
+                pixels[colorIndex++] = intensity; // Green
+                pixels[colorIndex++] = intensity; // Red
+                ++colorIndex;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+        KinectSensor sensor;
+        DepthFrameReader reader;
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.sensor = KinectSensor.GetDefault();
+            this.sensor.Open();
+
+            this.reader = this.sensor.DepthFrameSource.OpenReader();
+            this.reader.FrameArrived += OnFrameArrived;
+        }
+
+        void OnFrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    camera.Source = ProcessFrame(frame);
+                }
+            }
+        }
+      
     }
+        
 }
+

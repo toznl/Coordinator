d469bccdba21cbd87034ee51d7f3f95c6c363f17
Seo BoHyeong <dontsay_no@nate.com>
2018-03-29 16:25:30 +0900

    -Nothing

--- Coordinator/MainWindow.xaml.cs
+++ Coordinator/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+
+
 using Microsoft.Kinect;
 
 namespace Coordinator

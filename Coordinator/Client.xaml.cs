7b7ebbaaf1d36658a69caf7b062dac65a9a8d1aa
Seo BoHyeong <dontsay_no@nate.com>
2018-04-02 15:53:41 +0900

    -Change all skeleton Drawings to Comment

--- Coordinator/Client.xaml.cs
+++ Coordinator/Client.xaml.cs
@@ -21,10 +21,16 @@ using System.Runtime.InteropServices;
 
 namespace Coordinator
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct CoOrd
     {
+        [MarshalAs(UnmanagedType.R4)]
         public float x;
+
+        [MarshalAs(UnmanagedType.R4)]
         public float y;
+
+        [MarshalAs(UnmanagedType.R4)]
         public float z;
 
         public CoOrd(float x1, float y1, float z1)
@@ -33,15 +39,60 @@ namespace Coordinator
             y = y1;
             z = z1;
         }
-    }
 
+        // Calling this method will return a byte array with the contents
+        // of the struct ready to be sent via the tcp socket.
+        public byte[] Serialize()
+        {
+            // allocate a byte array for the struct data
+            var buffer = new byte[Marshal.SizeOf(typeof(CoOrd))];
+
+            // Allocate a GCHandle and get the array pointer
+            var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            var pBuffer = gch.AddrOfPinnedObject();
+
+            // copy data from struct to array and unpin the gc pointer
+            Marshal.StructureToPtr(this, pBuffer, false);
+            gch.Free();
+
+            return buffer;
+        }
+
+        // this method will deserialize a byte array into the struct.
+        public void Deserialize(ref byte[] data)
+        {
+            var gch = GCHandle.Alloc(data, GCHandleType.Pinned);
+            this = (CoOrd)Marshal.PtrToStructure(gch.AddrOfPinnedObject(), typeof(CoOrd));
+            gch.Free();
+        }
+
+        public void SendCoord(CoOrd co)
+        {
+            if (co.x != null && co.y != null && co.z != null)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    byte[] buffer = co.Serialize();
+
+                    TcpClient client = new TcpClient();
+                    client.Connect("127.0.0.1", 8080);
+                    Console.WriteLine("connected...");
+
+                    NetworkStream stream = client.GetStream();
+
+                    stream.Write(buffer, 0, buffer.Length);
+                    Console.WriteLine("{0} data sent", buffer.Length);
+                }
+
+            }
+        }
+    }
 
     public partial class Client : UserControl
     {
         private Socket socket = null;
         private Thread waitMsg = null;
         private Grid root;
-        CoOrd co = new CoOrd();
 
         bool flag = true;
         public Client(Grid root)
@@ -49,15 +100,7 @@ namespace Coordinator
             InitializeComponent();
             this.root = root;
             Connect();
-            //btSend.Click += new RoutedEventHandler(btSend_Click);
         }
-
-        //void btSend_Click(object sender, RoutedEventArgs e)
-        //{
-        //    //send 버튼 클릭
-        //    SendMsg();
-
-        //}
 
         private void SendMsg()
         {
